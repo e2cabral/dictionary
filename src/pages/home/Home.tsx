104e9e8dcc1dcd690c592b6f032991b3689f6d4c
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-23 21:42:50 -0300

    ✨ add a homepage component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import { WordList } from '../../components/WordList/WordList';
+import {WordList} from '../../components/WordList/WordList';
 
 import './Home.css';
+import {SearchBar} from "../../components/SearchBar/SearchBar";
+import {Word} from "../../domain/model/word-model";
 
-interface Props {
-
-}
+interface Props {}
 
 export const Home: React.FC<Props> = () => {
-    return (
-        <div className="container">
-            <div className="row">
-                <WordList words={[]} />
-            </div>
+  const [words, setWords] = useState<Array<Word>>([]);
+  const getWords: Function = (data: Array<Word>) => {
+    setWords(data);
+  }
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="searchbar__container">
+          <SearchBar setWords={getWords}/>
         </div>
-    );
+        <div className="wordlist__container">
+          <WordList words={words}/>
+        </div>
+      </div>
+    </div>
+  );
 }
