eb0e43d105507b41d067d84b6dd8aac25c50c55f
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-23 21:43:15 -0300

    ✨ add a word list component

--- src/components/WordList/WordList.tsx
+++ src/components/WordList/WordList.tsx
@@ -1,59 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import axios from '../../infra/http/axios-http-client/axios';
+import {Word} from "../../domain/model/word-model";
 
 import './WordList.css';
 
 interface Props {
-    words: Array<any>
+  words: Array<Word>;
 }
 
-export const WordList: React.FC<Props> = () => {
-    const [words, setWords] = useState<Array<Word>>([]);
-
-    const getWordList = async () => {
-        try {
-            const { data, status } = await axios.get<Array<Word>>('/');
-            if (status === 200) setWords(data);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    useEffect(() => {
-        getWordList();
-    }, []);
-
-    return (
-        <ul className="list">
-            {words.map((word) =>
-                <li className="listItem">
-                    <div className="listItem__container">
-                        <div className="listItem__header">
-                            <h3 className="listItem__word">
-                                { word.word }
-                            </h3>
-                            <h3 className="listItem__pos">
-                                { word.pos }
-                            </h3>
-                        </div>
-                        <p className="listItem__synonyms">
-                            { word.synonyms }
-                        </p>
-                        <p className="listItem__definition">
-                            { word.definitions.map((def) => <span>{ def }<br /></span>) }
-                        </p>
-                        <i className="listItem__icon"></i>
-                    </div>
-                </li>
-            )}
-        </ul>
-    );
-}
-
-export type Word = {
-    word: string,
-    pos: string,
-    synonyms: string,
-    definitions: Array<string>,
+export const WordList: React.FC<Props> = ({words}) => {
+  return (
+    <ul className="list">
+      {words.map((word, index) =>
+        <li key={index} className="listItem">
+          <div className="listItem__container">
+            <div className="listItem__header">
+              <h3 className="listItem__word">
+                {word.word}
+              </h3>
+              <h3 className="listItem__pos">
+                {word.pos}
+              </h3>
+            </div>
+            {word.synonyms !== undefined ? <p className="listItem__synonyms">{word.synonyms}</p> : <br/>}
+            <p className="listItem__definition">
+              {word.definitions.map((def) => <span>{def}<br/></span>)}
+            </p>
+          </div>
+        </li>
+      )}
+    </ul>
+  );
 }
